797b68285a68c21551eb9ff66e6ef6707059f4be
takato15 <gshawks21@gmail.com>
2021-09-11 20:01:04 +0000

    [update]create機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  resources :homes
+  resources :books
   get "top" => "homes#top"
-  get "books" => "books#index"
+  # get "books" => "books#index"
+  # post "books/creaate" => "books#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
