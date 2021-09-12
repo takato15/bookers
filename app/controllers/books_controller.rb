78c57aa548dd5a37b75915beca332a1f8fda7066
takato15 <gshawks21@gmail.com>
2021-09-12 18:31:52 +0000

    [update]update成功

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -24,8 +24,12 @@ class BooksController < ApplicationController
 
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(book)
+    else
+      redirect_to "/books"
+    end
   end
 
   def destroy
