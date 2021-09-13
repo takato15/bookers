1a6aba95f83e2c6167c8e694b8df03cb9bef7f97
takato15 <gshawks21@gmail.com>
2021-09-13 05:59:04 +0000

    [update]バリデーション機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,7 @@
 class BooksController < ApplicationController
   def index
-    @book = Book.all
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -8,14 +9,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
       flash[:notice] = "Book was successfully created."
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
-      redirect_to "/books"
+      render :index
     end
-    # redirect_to books_path(book.id)
   end
 
   def edit
@@ -23,12 +24,12 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
       flash[:notice] = "Book was successfully updated."
-      redirect_to book_path(book)
+      redirect_to book_path(@book)
     else
-      redirect_to "/books"
+      render :edit
     end
   end
 
